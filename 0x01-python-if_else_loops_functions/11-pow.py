b796a5b0d756a06e15dd186e7761c4d81ed3ddcf
jepez90 <ing.jersonperez@gmail.com>
2021-04-27 20:09:28 -0500

    [Update] now use the ** operator

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    power = 1
-    if b < 0:
-        for number in range(b, 0):
-            power /= a
-    else:
-        for number in range(0, b):
-            power *= a
-    return power
+    return a**b
