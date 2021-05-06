aad597429ec50a0482873882b67c15b8c21cfa98
jepez90 <ing.jersonperez@gmail.com>
2021-05-05 19:15:41 -0500

    [Downgrade]

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,24 +9,53 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
-	int i = 0, j = 0, numbers[1024] = {0};
+	int i = 0;
 
 	if (!head || !(*head))
 		return (1);
 
 	current = *head;
 	/* travel the list to found its length */
-	while (current)
+	while (current->next)
 	{
-		numbers[i++] = current->n;
 		current = current->next;
+		i++;
 	}
 
-	i--;
+	if ((*head)->n != current->n)
+		return (0);
 
-	while (j < i)
-		if (numbers[j++] != numbers[i--])
+	i -= 2;
+	current = (*head)->next;
+
+	while (i > 0)
+	{
+		/* if current is diferent of node in index i */
+		if (current->n != get_dnodeint_at_index(current, i)->n)
 			return (0);
-
+		current = current->next;
+		i -= 2;
+	}
 	return (1);
+}
+
+
+/**
+ * get_dnodeint_at_index - searcha the node with the gived index in the list.
+ * @head: pointer to the head of the double linked list.
+ * @index: node to be returned
+ *
+ * Return: node at index or NULL.
+ */
+listint_t *get_dnodeint_at_index(listint_t *head, unsigned int index)
+{
+	while (head)
+	{
+		if (index-- == 0)
+			return (head);
+
+		head = head->next;
+	}
+
+	return (NULL);
 }
