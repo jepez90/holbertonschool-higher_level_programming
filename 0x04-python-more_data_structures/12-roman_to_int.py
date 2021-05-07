2da28d4b7a624ea14db5040b78239ffe1722f5c2
jepez90 <ing.jersonperez@gmail.com>
2021-05-06 23:18:14 -0500

    [Update] for reability

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def val_of_symbol(symbol):
+def roman_to_int(roman_string):
+    integer = 0
     equivalency = {
         'I': 1,
         'V': 5,
@@ -9,18 +10,12 @@ def val_of_symbol(symbol):
         'D': 500,
         'M': 1000,
     }
-    return equivalency[symbol]
+    if roman_string and type(roman_string) == str:
 
-
-def roman_to_int(roman_string):
-    integer = 0
-
-    if roman_string is not None and type(roman_string) == str:
-
-        equ_values = [val_of_symbol(l) for l in roman_string]
+        equ_values = [equivalency[l] for l in roman_string]
 
         for i, value in enumerate(equ_values):
-            if (i + 1 != len(equ_values) and max(equ_values[i + 1:]) > value):
+            if (len(equ_values[i + 1:]) and max(equ_values[i + 1:]) > value):
                 integer -= value
             else:
                 integer += value
