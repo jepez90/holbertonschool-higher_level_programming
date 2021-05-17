6bbf86af51647e9eed84fd98520c1475af03ac76
jepez90 <ing.jersonperez@gmail.com>
2021-05-17 15:20:52 -0500

    [Fix] error when x equals to 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for index in range(0, x):
+    index = 0
+
+    while (index < x):
+        try:
             print(my_list[index], end='')
-    except IndexError:
-        return index
-    finally:
-        print()
-    return index + 1
+            index += 1
+        except IndexError:
+            break
+    print()
+    return index
