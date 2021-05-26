47794001bafb18101fff1590ace87631890f074a
jepez90 <ing.jersonperez@gmail.com>
2021-05-26 12:01:16 -0500

    [Fix] change the test when string is pased

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,14 +11,15 @@ class TestMaxInteger(unittest.TestCase):
     """
     def test_max_integer(self):
         # test max integer when exist many types in the list
+        
+        self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer([1, 2, 5, 4]), 5)
         self.assertAlmostEqual(max_integer([1, 2, 5, 5.6]), 5.6)
-        self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer([-3, -4]), -3)
+        self.assertAlmostEqual(max_integer('Hola'), 'o')
 
         # test for rise correct errors
         self.assertRaises(TypeError, max_integer, [10, '10'])
         self.assertRaises(TypeError, max_integer, [10, 's'])
-        self.assertRaises(TypeError, max_integer, {'a': 10, 'b': 12})
-        self.assertRaises(TypeError, max_integer, 'hola')
+        self.assertRaises(KeyError, max_integer, {'a': 10, 'b': 12})
         self.assertRaises(TypeError, max_integer, None)
