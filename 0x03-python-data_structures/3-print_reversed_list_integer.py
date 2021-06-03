a349fec69a46e0adc695107f9ced4568d2a2a0fe
jepez90 <ing.jersonperez@gmail.com>
2021-06-02 22:12:58 -0500

    [Add] remove check for type

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if type(my_list) == list:
-        for number in reversed(my_list):
-            print("{:d}".format(number))
+    for number in reversed(my_list):
+        print("{:d}".format(number))
