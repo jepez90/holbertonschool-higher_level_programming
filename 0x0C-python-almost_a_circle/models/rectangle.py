13207cc0753c8a396353937e18c81240a127a79e
jerson <ing.jersonperez@gmail.com>
2021-06-08 15:34:53 -0500

    [Update] remove checks in uptdate function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,17 +83,16 @@ class Rectangle(Base):
         """ change the properties by way list or dictionary
         if only values are passed, it will be interpreted as in __properties
         """
-        if args is not None and type(args) is tuple and len(args) != 0:
+        if args is not None and len(args) != 0:
             # search the arg name in __properties class attribute
             for i, arg in enumerate(args):
                 if i < len(type(self).PROPERTIES):
                     setattr(self, type(self).PROPERTIES[i], arg)
         else:
             if kwargs is not None:
-                if type(kwargs) is dict and len(kwargs) != 0:
-                    for arg_name, arg in kwargs.items():
-                        if arg_name in type(self).PROPERTIES:
-                            setattr(self, arg_name, arg)
+                for arg_name, arg in kwargs.items():
+                    if arg_name in type(self).PROPERTIES:
+                        setattr(self, arg_name, arg)
 
     def to_dictionary(self):
         """ that returns the dictionary representation of a Rectangle """
