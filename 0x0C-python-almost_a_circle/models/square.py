c5e06914380023989f0304db5f7c2f10f66202bd
jerson <ing.jersonperez@gmail.com>
2021-06-08 15:40:04 -0500

    [Update] override update method in square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,19 @@ class Square(Rectangle):
     def __str__(self):
         txt = "[Square] ({}) {}/{} - {}"
         return txt.format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """ change the properties by way list or dictionary
+        if only values are passed, it will be interpreted as in __properties
+        """
+        if args is not None and type(args) is tuple and len(args) != 0:
+            # search the arg name in __properties class attribute
+            for i, arg in enumerate(args):
+                if i < len(type(self).PROPERTIES):
+                    setattr(self, type(self).PROPERTIES[i], arg)
+        else:
+            if kwargs is not None:
+                if type(kwargs) is dict and len(kwargs) != 0:
+                    for arg_name, arg in kwargs.items():
+                        if arg_name in type(self).PROPERTIES:
+                            setattr(self, arg_name, arg)
