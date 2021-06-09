4176037fa0950c6ee6219ac825b7d6674da806f3
jepez90 <ing.jersonperez@gmail.com>
2021-06-08 21:44:38 -0500

    [Update] some things

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -75,9 +75,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ obtains the string json representation from an object  """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return '[]'
-        return json.dumps(list_dictionaries, sort_keys=True)
+        return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
