2c37a88911f67d0684d159478afc42435ac61359
jepez90 <ing.jersonperez@gmail.com>
2021-06-08 21:31:54 -0500

    [Update] change condition from len to json_string == []

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -79,7 +79,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or len(json_string) == 0:
+        if json_string is None or json_string == []:
             return []
         else:
             return json.loads(json_string)
