6de845cf563b61816bbca64d34db43e60b23059c
Jerson Perez <ing.jersonperez@gmail.com>
2021-08-03 09:26:08 -0500

    [Fix] style error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   let arrayNumbers = process.argv.slice(2);
-  arrayNumbers = arrayNumbers.sort((a, b) => { return a - b });
+  arrayNumbers = arrayNumbers.sort((a, b) => { return a - b; });
   console.log(arrayNumbers.reverse()[1]);
 }
