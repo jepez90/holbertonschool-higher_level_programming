f303de4f0667b2d87ae954e90931b51626cd8f5a
jepez90 <ing.jersonperez@gmail.com>
2021-08-19 17:58:57 -0500

    [update] adding the silent mode to curl

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL,and displays the size of the body of the response
-curl -X DELETE "$1"
+curl -Xs DELETE "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL,and displays the size of the body of the response
-curl -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -Hs "X-HolbertonSchool-User-Id: 98" "$1"
