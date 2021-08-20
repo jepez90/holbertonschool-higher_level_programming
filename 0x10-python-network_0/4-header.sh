98613ae3c9f03536f49f1005c3d6fde906913fbe
jepez90 <ing.jersonperez@gmail.com>
2021-08-19 20:32:41 -0500

    [Fix] error with the correct position of argument -s

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL,and displays the size of the body of the response
-curl -Hs "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
