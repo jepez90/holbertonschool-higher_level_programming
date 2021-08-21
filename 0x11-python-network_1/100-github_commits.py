357342a7a732b972240d539c5d8bccd04d796867
jepez90 <ing.jersonperez@gmail.com>
2021-08-20 21:09:28 -0500

    [Add] file for advance task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" takes 2 arguments in order and list 10 commits
+(from the most recent to oldest) of the repository “rails” by the user “rails”
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    data = {"per_page": 10
+            }
+
+    response = requests.get(url, data)
+    for commit in response.json():
+        # print(commit["commit"]["author"]["date"], end=" ")
+        print("{}: {}".format(commit["sha"],
+              commit["commit"]["author"]["name"]))
