352866996f494cc0e953f1b6a0ea36433ea8e955
jepez90 <ing.jersonperez@gmail.com>
2021-08-23 10:28:20 -0500

    [Update] remove args to the call

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,12 +10,10 @@ import sys
 if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10".format(owner, repo)
 
-    data = {"per_page": "10"}
+    response = requests.get(url)
 
-    response = requests.get(url, params=data)
-    print(response.url)
     for commit in response.json():
         sha = commit.get("sha")
         author = commit.get("commit").get("author").get("name")
